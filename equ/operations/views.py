edd8ee221da72f4d41ddfe97329b1987514cb6be
Erasyl <erasil.bakhytgan@gmail.com>
2024-10-13 19:35:39 +0600

    create def in views operations

--- equ/operations/views.py
+++ equ/operations/views.py
@@ -1,3 +1,9 @@
+from logging import LogRecord
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def create_operation_log(request):
+    return render(request, template_name="")
