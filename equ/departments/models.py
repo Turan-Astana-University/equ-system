731d1e562d3db0e08bcd49ec386d20f92325379c
Erasil <erasil.bakhytgan@gmail.com>
2024-10-15 17:46:22 +0500

    update template and models:

--- equ/departments/models.py
+++ equ/departments/models.py
@@ -5,3 +5,6 @@ from django.db import models
 
 class Department(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- equ/positions/models.py
+++ equ/positions/models.py
@@ -5,3 +5,6 @@ from django.db import models
 
 class Position(models.Model):
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
