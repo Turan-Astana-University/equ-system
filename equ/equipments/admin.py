0bda7e3bc0528d3bfdb730ba7f005339384657c9
Erasil <erasil.bakhytgan@gmail.com>
2024-11-25 12:01:54 +0500

    add multiple cartridge

--- equ/equipments/admin.py
+++ equ/equipments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Equipment, EquipmentType, Cartridge, CartridgeTypes
+from .models import Equipment, EquipmentType, Cartridge, CartridgeTypes, CategoryChoices
 # Register your models here.
 
 
@@ -10,12 +10,38 @@ class EquipmentAdmin(admin.ModelAdmin):
 
 admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(EquipmentType)
+#
+#
+# class CartridgeAdmin(admin.ModelAdmin):
+#     list_display = ['title']
+#     readonly_fields = ('barcode', 'date_last_check', 'date_last_invent')
+#
 
 
+@admin.register(Cartridge)
 class CartridgeAdmin(admin.ModelAdmin):
-    list_display = ['title']
-    readonly_fields = ('barcode', 'date_last_check', 'date_last_invent')
+    list_display = ['color', 'status', 'cartridge_type']
+
+    # Кастомное действие
+    actions = ['create_multiple_cartridges']
+
+    def create_multiple_cartridges(self, request, queryset):
+        # Создать 10 новых картриджей с одинаковым параметром
+        cartridge_type = CartridgeTypes.objects.first()  # Замените на нужный тип
+        cartridges = [
+            Cartridge(
+                color="Black",
+                status=CategoryChoices.NEW,
+                cartridge_type=cartridge_type
+            )
+            for _ in range(10)  # Укажите количество записей
+        ]
+        Cartridge.objects.bulk_create(cartridges)
+        self.message_user(request, f"{len(cartridges)} новых картриджей успешно созданы.")
+
+    create_multiple_cartridges.short_description = "Создать 10 новых картриджей"
 
 
+admin.site.unregister(Cartridge)
 admin.site.register(Cartridge, CartridgeAdmin)
 admin.site.register(CartridgeTypes)
