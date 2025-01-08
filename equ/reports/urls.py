bb78fcc9717a832bc673eb91130d52b14db7e591
Serik <serik-01.kz@mail.ru>
2025-01-08 13:46:10 +0500

    filter

--- equ/reports/urls.py
+++ equ/reports/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import EquipmentReportView, PrinterReportView, InventoryReportView
-from . import views
+
 urlpatterns = [
     path("report_equipments/", EquipmentReportView.as_view(), name="report_equipments"),
     path("report_printers/", PrinterReportView.as_view(), name="report_printers"),
     path("report_inventorys/", InventoryReportView.as_view(), name="report_inventorys"),
-    path('export-excel/', views.export_excel, name='export_excel'),
 ]
