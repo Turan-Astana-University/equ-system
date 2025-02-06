c60be9f0ea4fdce063af502bfebd540f0a442a0b
Erasil <erasil.bakhytgan@gmail.com>
2025-02-06 13:17:15 +0500

    f

--- equ/equ/settings.py
+++ equ/equ/settings.py
@@ -155,14 +155,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
+EMAIL_HOST_USER = 'turan.astana.university@yandex.ru'
+EMAIL_HOST_PASSWORD = 'rladvgvvqwjpfdaj'
 EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'turan.astana.university@yandex.com'
-EMAIL_HOST_PASSWORD = 'towcbkthgqyhpkjo'
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # myproject/settings.py
