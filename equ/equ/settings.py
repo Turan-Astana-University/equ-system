ab7d8cc078d6091b012a99da00afe7cf6c9e11f0
Erasil <erasil.bakhytgan@gmail.com>
2025-02-25 13:11:20 +0500

    g

--- equ/equ/settings.py
+++ equ/equ/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'equ.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'equipment-system',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
